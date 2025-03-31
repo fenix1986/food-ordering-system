84a9af1efcfbad5ecdac036cddf10a5b62b90a40
Jakub Prabucki <jakub.prabucki@gmail.com>
2025-03-31 15:27:39 +0200

    Add Customer and Restaurant entity classes

---
+++ order-service/order-domain/order-domain-core/src/main/java/pl/metaclass/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,7 @@
+package pl.metaclass.ordering.system.order.service.domain.entity;
+
+import pl.metaclass.ordering.system.domain.entity.AggregateRoot;
+import pl.metaclass.ordering.system.domain.valueobject.CustomerId;
+
+public class Customer extends AggregateRoot<CustomerId> {
+}
