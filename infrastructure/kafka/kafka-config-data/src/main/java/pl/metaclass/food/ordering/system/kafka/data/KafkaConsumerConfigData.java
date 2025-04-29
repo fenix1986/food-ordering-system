0316bb8d7a6c164a7b839edcd8f391107c676474
Jakub Prabucki <jakub.prabucki@gmail.com>
2025-04-29 08:52:02 +0200

    Update Kafka configuration and add new modules for Kafka producer, consumer, and config data

---
+++ infrastructure/kafka/kafka-config-data/src/main/java/pl/metaclass/food/ordering/system/kafka/data/KafkaConsumerConfigData.java
@@ -0,0 +1,28 @@
+package pl.metaclass.food.ordering.system.kafka.data;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-consumer-config")
+class KafkaConsumerConfigData {
+	private String keyDeserializer;
+	private String valueDeserializer;
+	private String autoOffsetReset;
+	private String specificAvroReaderKey;
+	private String specificAvroReaderValue;
+	private Boolean batchListener;
+	private Boolean autoStartup;
+	private Integer concurrencyLevel;
+	private Integer sessionTimeoutMs;
+	private Integer heartbeatIntervalMs;
+	private Integer maxPollIntervalMs;
+	private Long pollTimeoutMs;
+	private Integer maxPollRecords;
+	private Integer maxPartitionFetchBytesDefault;
+	private Integer maxPartitionFetchBytesBoostFactor;
+
+
+}
---
+++ infrastructure/kafka/kafka-config-data/src/main/java/pl/metaclass/food/ordering/system/kafka/data/KafkaProducerConfigData.java
@@ -0,0 +1,20 @@
+package pl.metaclass.food.ordering.system.kafka.data;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "kafka-producer-config")
+class KafkaProducerConfigData {
+	private String keySerializerClass;
+	private String valueSerializerClass;
+	private String compressionType;
+	private String acks;
+	private Integer batchSize;
+	private Integer batchSizeBoosFactor;
+	private Integer lingerMs;
+	private Integer requestTimeoutMs;
+	private Integer retryCount;
+}
